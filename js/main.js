a1fd71ceabf1996075b7f2ace84ab414611910c4
jskornyakova <jskornyakova@gmail.com>
2019-06-02 22:48:00 +0300

    add comment for classes cart and cartItems

--- js/main.js
+++ js/main.js
@@ -1,21 +1,78 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 30},
-    {id: 3, title: 'Keyboard', price: 55},
-    {id: 4, title: 'Gamepad', price: 75},
-];
+class ProductsList {
+    constructor(container = '.products'){
+        this.container = container;
+        this.data = []; // получаем данные от сервера
+        this.allProducts = [];
+        this.init();
+    }
+    init(){
+        this._fetchProducts();
+        this._render();
+        this._costProductList(this.allProducts);
+    }
+    _fetchProducts(){ //условно входные данные от сервера
+        this.data = [
+            {id: 1, title: 'Notebook', price: 2000},
+            {id: 2, title: 'Mouse', price: 30},
+            {id: 3, title: 'Keyboard', price: 55},
+            {id: 4, title: 'Gamepad', price: 65},
+        ];
+    }
+    _render(){ //добавляем каждый элемент продукта в каталог
+        const block = document.querySelector(this.container);
+        for (let item of this.data){
+            const product = new ProductItem(item);
+            this.allProducts.push(product);
+            block.insertAdjacentHTML('beforeend', product.render());
+        }
+    }
+    _costProductList(items) {
+        var cost = 0;
+        for (let i = 0; i < items.length; i++) {
+            cost += items[i].price
+        }
+        console.log('Товаров на сумму ' + cost);
+    }
+}
 
-const renderProduct = (title, price) => { //возвращает разметку одного товара
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price} $</p>
-                <button class="buy-btn">Купить</button>
-            </div>`
-};
+class ProductItem {
+    constructor(product, img = `https://placehold.it/200x150`){
+        this.id = product.id;
+        this.title = product.title;
+        this.price = product.price;
+        this.img = img;
+    }
+    render(){ //рисуем карточку товара
+        return `<div class="product-item"> 
+                 <img src="${this.img}" alt="${this.title}">
+                 <div class="desc">
+                     <h3>${this.title}</h3>
+                     <p>${this.price}</p>
+                     <button class="buy-btn">Купить</button>
+                 </div>
+             </div>`
+    }
+}
 
-const renderPage = list => { // принимать список всех товаров, list-массив, map-перебирает каждый элемент и возвращает значение
-    const productsList = list.map(item => renderProduct(item.title, item.price)); //item - аргумент
-    document.querySelector('.products').innerHTML = productsList.join('');
-};
+class Cart {
+    constructor() {
+       // this.allCart = []; массив для хранения всего добавленного в корзину товара
+    }
+    // render() {} рендерим структуру всей корзины
+    // costCart() {} стоимость корзины
+    // clearCart() {} полностью очистить корзину
+}
+class CartItem {
+    constructor(){
+        //  this.id = product.id; индификатор
+        //  this.title = product.title; название
+        //  this.price = product.price; цена
+        //  this.img = img; картинка
+        //  this.quantity = 1; количество
+    }
+    // render() {} рендерим структуру элемента корзины
+    // addInCart () {} добавить в корзину ещё 1 ед. товара
+    // delFromCart () {} убрать товар из корзины как 1 ед., так и весь товар
+}
 
-renderPage(products);
+const products = new ProductsList();
